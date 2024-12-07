865f9a3c7460dc8f67dd6c79fc056b7432387118
regina-danilchenko <reginadanilcenko3@gmail.com>
2024-12-07 19:32:49 +0600

    add random color

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import sys
 
 from random import randrange
-from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 class YellowCircles(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.initUI()
 
+    def initUI(self):
+        self.setGeometry(0, 0, 800, 600)
+        self.setWindowTitle('Первая программа')
         self.do_paint = False
+        self.button_draw_circle = QPushButton(self)
+        self.button_draw_circle.setText('Окружность')
+        self.button_draw_circle.resize(80, 40)
+        self.button_draw_circle.move(720, 560)
         self.button_draw_circle.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -28,7 +34,7 @@ class YellowCircles(QMainWindow):
 
     def draw_circle(self, qp):
         r = randrange(600)
-        qp.setPen(QColor(255, 255, 0))
+        qp.setPen(QColor(randrange(255), randrange(255), randrange(255)))
         qp.drawEllipse(10, 20, r, r)
 
 
