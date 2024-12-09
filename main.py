5b58891445b02d5952a20852403880978cc15b2b
regina-danilchenko <reginadanilcenko3@gmail.com>
2024-12-09 20:55:09 +0600

    add ui from py-file

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import sys
 
+import io
+from PyQt6 import uic
 from random import randrange
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from UI import template
 
 
 class YellowCircles(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(0, 0, 800, 600)
-        self.setWindowTitle('Первая программа')
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
         self.do_paint = False
-        self.button_draw_circle = QPushButton(self)
-        self.button_draw_circle.setText('Окружность')
-        self.button_draw_circle.resize(80, 40)
-        self.button_draw_circle.move(720, 560)
         self.button_draw_circle.clicked.connect(self.paint)
 
     def paintEvent(self, event):
